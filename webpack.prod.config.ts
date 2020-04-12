b7062383ef4dc929b4e65813fb9634ef043eda91
himanshu-nath <hnath723@gmail.com>
2020-04-12 23:17:44 +0530

    test

---
+++ webpack.prod.config.ts
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: ["webpack/hot/poll?100", "./src/server.ts"],
+  target: "node",
+  externals: [
+    nodeExternals({
+      whitelist: ["webpack/hot/poll?100"]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "server.js"
+  }
+};
